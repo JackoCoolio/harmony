cb9dd01f200972fb1788c65415eab70d70c57ebc
Jackson Wambolt <jtwambolt@gmail.com>
2020-08-31 23:03:47 -0500

    Separate sprite from batch code

---
+++ src/harmony_sprite.h
@@ -0,0 +1,12 @@
+#ifndef HARMONY_SPRITE_H
+#define HARMONY_SPRITE_H
+
+#include "harmony_texture.h"
+
+typedef struct harmony_sprite
+{
+    unsigned int index;
+    harmony_texture_t texture;
+} harmony_sprite_t;
+
+#endif
