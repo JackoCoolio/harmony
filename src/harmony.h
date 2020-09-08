78edc1a88fdac93ef8b36c8a4ffdcac2925d0e23
Jackson Wambolt <jtwambolt@gmail.com>
2020-09-07 20:29:03 -0500

    Update harmony.h

--- src/harmony.h
+++ src/harmony.h
@@ -2,28 +2,31 @@
 extern "C"
 {
 #endif
-
+    
 #define GLEW_STATIC
-
-// Harmony Core
+    
+    // Harmony Core
 #include "harmony_context.h"
-
-// Data types & general utilities
+    
+    // Data types & general utilities
 #include "harmony_vector.h"
 #include "harmony_linked_list.h"
 #include "harmony_timer.h"
 #include "harmony_file.h"
-
-// Graphics
+#include "harmony_buffer.h"
+#include "harmony_log.h"
+    
+    // Graphics
 #include "harmony_shader.h"
 #include "harmony_renderer.h"
 #include "harmony_texture.h"
-
-// Entry point
+#include "harmony_batch.h"
+    
+    // Entry point
 #include "harmony_entry.h"
-
+    
 #define main harmony_createApplication
-
+    
 #ifdef __cplusplus
 }
 #endif
